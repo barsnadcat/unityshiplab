0f7ab95f328c10ed88fc555ffa3cc1515b02e412
barsandcat <barsandcat@gmail.com>
2014-07-21 08:53:40 +0300

    More cleanup

--- Assets/InventoryGUI.cs
+++ Assets/InventoryGUI.cs
@@ -3,13 +3,12 @@ using System.Collections;
 
 public class InventoryGUI : MonoBehaviour
 {
-	private int toolbarInt = 0;
-	private string[] toolbarStrings = {"Toolbar1", "Toolbar2", "Toolbar3"};
-		
 	void OnGUI ()
 	{
-		toolbarInt = GUI.Toolbar (new Rect (25, 25, 250, 30), toolbarInt, toolbarStrings);
+		if (GUI.Button (Rect (20,40,80,20), "Level 1")) 
+		{
+			Application.LoadLevel (1);
+		}
 	}
-		
 }
 
